f73548738144709e3219321be235e8251069a3ac
nshariatmogh <negin.sadat.shariat.moghani@zalando.de>
2021-12-13 19:40:41 +0100

    pom_setup

---
+++ src/main/java/com/javaExample/starterfirst/StarterFirstApplication.java
@@ -0,0 +1,13 @@
+package com.javaExample.starterfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterFirstApplication.class, args);
+	}
+
+}
