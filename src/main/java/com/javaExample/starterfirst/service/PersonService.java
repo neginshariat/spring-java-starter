9ad1bbecb9b268c8e844e5e90d439b72a9ff685c
nshariatmogh <negin.sadat.shariat.moghani@zalando.de>
2021-12-13 22:03:56 +0100

    add-Controller

---
+++ src/main/java/com/javaExample/starterfirst/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.javaExample.starterfirst.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.javaExample.starterfirst.dao.PersonDao;
+import com.javaExample.starterfirst.model.Person;
+
+@Service
+public class PersonService {
+	
+	private final PersonDao personDao;
+	
+	@Autowired
+	public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+		this.personDao = personDao;
+	}
+
+	public int addPerson(Person person) {
+		return personDao.insertPerson(person);
+	}
+
+}
