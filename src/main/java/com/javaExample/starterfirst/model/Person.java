bdb9b1683aa3c6a543136706a3734e4112bd93e2
nshariatmogh <negin.sadat.shariat.moghani@zalando.de>
2022-01-10 14:56:39 +0100

    dao-test-layer

--- src/main/java/com/javaExample/starterfirst/model/Person.java
+++ src/main/java/com/javaExample/starterfirst/model/Person.java
@@ -2,9 +2,12 @@ package com.javaExample.starterfirst.model;
 
 import java.util.UUID;
 
+import org.springframework.lang.NonNull;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Person {
+	@NonNull
 	private UUID id;
 	
 	/*
@@ -21,10 +24,19 @@ public class Person {
 	public UUID getId() {
 		return id;
 	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getName() {
 		return name;
 	}
-	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
 	
 	
 
---
+++ src/test/java/com/javaExample/starterfirst/api/PersonControllerTest.java
@@ -0,0 +1,16 @@
+package com.javaExample.starterfirst.api;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+import com.javaExample.starterfirst.StarterFirstApplication;
+
+
+
+
+
+@SpringBootTest(classes = StarterFirstApplication.class)
+@WebAppConfiguration
+class PersonControllerTest {
+
+
+	
+}
